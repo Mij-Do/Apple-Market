9991ac92c7a8ce8136cf6b9e6cb9ca8985f64e2f
ahmed <ahmedmijdo2@gmail.com>
2025-01-08 13:01:10 +0200

    add products section

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,9 @@
 let header = document.getElementById("header");
 let imgShow = document.getElementById("showImg");
-
+let selectIph = document.getElementById("select-Iph");
+let aColor = document.querySelectorAll("a");
+let btn = document.getElementById("btn");
+let footerImg = document.getElementById("footerImg");
 
 function setColor (color) {
     localStorage.setItem("color", color);
@@ -14,21 +17,34 @@ function srcCode (src) {
 function headerColor (backColor) {
     localStorage.setItem("backColor", backColor);
     header.style.backgroundColor = backColor;
+    selectIph.style.backgroundColor = backColor;
+    footerImg.style.backgroundColor = backColor;
 }
 function textColor (txt) {
     localStorage.setItem("textColor", txt);
-    // aColor.style.color = txt;
     document.body.style.color = txt;
+    aColor.forEach(element => {
+        element.style.color = txt;
+    });
 }
 
 if (localStorage.length > 0) {
     document.body.style.backgroundColor = localStorage.color;
     imgShow.src = localStorage.src;
     header.style.backgroundColor = localStorage.backColor;
-    // aColor.style.color = localStorage.textColor;
+    selectIph.style.backgroundColor = localStorage.backColor;
     document.body.style.color = localStorage.textColor;
+    aColor.forEach(element => {
+        element.style.color = localStorage.textColor;
+    });
+    footerImg.style.backgroundColor = localStorage.backColor;
 } else {
     imgShow.src = "./Images/Iphone natural/natural.png";
 }
+
+// date
+let date = new Date();
+let dateNow = document.getElementById("dateNow");
+dateNow.textContent = date.getFullYear();
 
 
